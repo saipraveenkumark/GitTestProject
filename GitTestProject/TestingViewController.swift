31f96957eb23e52a68d13a64cda9ec49a49e9a74
mangi-reddy58 <mangi-_reddy@simplicity-creations.com>
2017-12-28 14:52:06 +0530

    ChangeOne

---
+++ GitTestProject/TestingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TestingViewController.swift
+//  GitTestProject
+//
+//  Created by Rushikesh Kulkarni on 28/12/17.
+//  Copyright © 2017 Sai. All rights reserved.
+//
+
+import UIKit
+
+class TestingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
